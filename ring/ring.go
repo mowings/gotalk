9f1d2473f4a491b176c052a26ed9f57c14f74b6f
Michael Owings <mowings@turbosquid.com>
2015-11-07 17:23:53 -0600

    more demo

--- ring/ring.go
+++ ring/ring.go
@@ -9,16 +9,21 @@ import (
 const NUM_ROUTINES = 2000000
 
 func main() {
+
+	// Get worker count and passes from command line
 	if len(os.Args) <= 2 {
 		fmt.Println("Missing argument.")
 		return
 	}
 	num_routines, _ := strconv.Atoi(os.Args[1])
 	passes, _ := strconv.Atoi(os.Args[2])
+
+	// Set up ring head and tail
 	start := make(chan int)
 	finish := make(chan int)
 	in := start
 
+	// Create goroutines and channels
 	fmt.Printf("Creating %d goroutines\n", num_routines)
 	for i := 1; i <= num_routines; i++ {
 		out := make(chan int)
@@ -34,6 +39,7 @@ func main() {
 		in = out
 	}
 
+	// Send token around ring
 	fmt.Printf("Sending token around ring\n")
 	n := 0
 	for p := 0; p < passes; p++ {
